3ea904c58fb1043fdac169b47e5236139be997fb
Alexander Novitskiy <alexandernovitsky77@gmail.com>
2023-01-30 23:21:10 +0200

    Load environment variables from os

--- src/common/configuration.py
+++ src/common/configuration.py
@@ -1,6 +1,7 @@
 from json import load
 from dotenv import dotenv_values
+from os import environ
 
-secrets = dotenv_values(".env")
+secrets = {**dotenv_values(".env"), **environ}
 
 configuration = dict(**load(open("appsettings.json", encoding="UTF-8")), **secrets)
